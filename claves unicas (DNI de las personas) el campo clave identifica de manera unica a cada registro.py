c3774cd3c24883d84941f49242392ada7c4367e2
GenaroHacker <genarominetto@hotmail.com>
2022-07-07 00:48:09 +0200

    instruccion SQL multilinea   y    PRIMARY KEY

--- claves unicas (DNI de las personas) el campo clave identifica de manera unica a cada registro.py
+++ claves unicas (DNI de las personas) el campo clave identifica de manera unica a cada registro.py
@@ -5,10 +5,22 @@ miConexion=sqlite3.connect("GestionProductos")
 miCursor=miConexion.cursor()
 
 
+miCursor.execute('''
+    CREATE TABLE PRODUCTOS (
+    CODIGO_ARTICULO VARCHAR(5) PRIMARY KEY,
+    NOMBRE_ARTICULO VARCHAR(50),
+    PRECIO INTEGER,
+    SECCION VARCHAR(20))
+''')
 
+productos=[
+    ("AR01", "pelota", 10, "jugueteria"),
+    ("AR02", "pantalon", 20, "confeccion"),
+    ("AR03", "zapatos", 30, "accesorios"),
+    ("AR04", "camiseta", 40, "deportes"),
+]
 
-
-
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?,?)", productos)
 
 miConexion.commit()
 
