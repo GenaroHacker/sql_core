f63fb37dc93361d1a791f62c52c4369b034ba6c3
GenaroHacker <genarominetto@hotmail.com>
2022-07-06 23:56:55 +0200

    ejecutar lote de registros

--- ejecutar lote de registros.py
+++ ejecutar lote de registros.py
@@ -1,21 +1,27 @@
 import sqlite3
 
-# Abrir-Crear conexión
 miConexion=sqlite3.connect("PrimeraBase")
 
-# Crear puntero
 miCursor=miConexion.cursor()
 
-# Ejecutar query (consulta) SQL
-# Insertar, Leer, Actualizar, Borrar (Create, Read, Update, Delete)
-
-# (Tabla ya creada)
 #miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
-# (Elemento ya insertado)
+
 #miCursor.execute("INSERT INTO PRODUCTOS VALUES ('BALÓN', 10, 'DEPORTE')")
 
-# Confirmar cambios
+variosProductos=[
+
+    ("Camiseta", 10, "Deporte"),
+    ("Jarrón", 20, "Cerámica"),
+    ("Pelota", 5, "Deporte")
+
+
+]
+
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)", variosProductos)
+
 miConexion.commit()
 
-# Cerrar conexión
+
+
+
 miConexion.close()
