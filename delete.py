997ccbc68042541ae31daacfdae629d93ff379ba
GenaroHacker <genarominetto@hotmail.com>
2022-07-07 01:54:01 +0200

    delete

--- delete.py
+++ delete.py
@@ -2,10 +2,12 @@ import sqlite3
 
 miConexion=sqlite3.connect("GestionProductos3")
 
-
 miCursor=miConexion.cursor()
 
-miCursor.execute("UPDATE PRODUCTOS SET NOMBRE_ARTICULO='Pelota' WHERE ID=1")
+
+miCursor.execute("DELETE FROM PRODUCTOS WHERE ID=2")
+
+
 
 
 miConexion.commit()
