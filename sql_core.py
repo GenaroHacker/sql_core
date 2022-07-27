d0f02c6d18f8258048fe21b5b0a84fa3f72bb5d5
GenaroHacker <genarominetto@hotmail.com>
2022-07-27 12:02:37 +0200

    test improved

--- sql_core.py
+++ sql_core.py
@@ -32,6 +32,14 @@ def ReadRecords(DATABASE_NAME, TABLE_NAME):
     myConnection.close()
     return records
 
+def ReadLastRecord(DATABASE_NAME, TABLE_NAME):
+    myConnection=sqlite3.connect(DATABASE_NAME)
+    myCursor=myConnection.cursor()
+    myCursor.execute("SELECT * FROM " + TABLE_NAME + " ORDER BY ID DESC LIMIT 1")
+    records=myCursor.fetchall()
+    myConnection.close()
+    return records[0]
+
 def UpdateRecord(DATABASE_NAME, RECORD):
     myConnection=sqlite3.connect(DATABASE_NAME)
     myCursor=myConnection.cursor()
@@ -56,25 +64,50 @@ def RunCommand(DATABASE_NAME, COMMAND):
 
 
 if __name__ == "__main__":
+    #Create one Table
+    try:
+        columns = """
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                ITEM_NAME VARCHAR(50),
+                PRICE INTEGER,
+                SECTION VARCHAR(20)"""
+        CreateTable("BaseProducts","TableProducts",columns)
+        print("Table created successfully!\n")
+    except sqlite3.OperationalError:
+        print("Table already exists!!!!\n")
 
-    columns = """
-            ARTICLE_CODE INTEGER PRIMARY KEY AUTOINCREMENT,
-            ITEM_NAME VARCHAR(50),
-            PRICE INTEGER,
-            SECTION VARCHAR(20)"""
-    CreateTable("BaseProducts","TableProducts",columns)
 
+    #Insert one record
     InsertRecord("BaseProducts","INSERT INTO TableProducts VALUES (NULL,'BALL',10,'SPORT')")
+    print("Record inserted successfully!\n")
 
+
+    #Insert several records
     InsertSeveralRecords("BaseProducts",[
         "INSERT INTO TableProducts VALUES (NULL,'GOLF STICK',25,'SPORT')",
         "INSERT INTO TableProducts VALUES (NULL,'GLASS',20,'CERAMIC')",
         "INSERT INTO TableProducts VALUES (NULL,'T-SHIRT',5,'CLOTHES')"
     ])
+    print("Records inserted successfully!\n")
 
+
+    #Read all records
     list_of_tuples = ReadRecords("BaseProducts","TableProducts")
+    print("The list of tuples with all the records is:")
     print(list_of_tuples)
 
-    UpdateRecord("BaseProducts","UPDATE TableProducts SET ITEM_NAME='BALL NAME UPDATED' WHERE ARTICLE_CODE=3")
 
-    RemoveRecord("BaseProducts","DELETE FROM TableProducts WHERE ARTICLE_CODE=3")
+    #Read last record
+    last_record = ReadLastRecord("BaseProducts","TableProducts")
+    print("\nThe last record is:")
+    print(last_record)
+
+
+    #Update record
+    UpdateRecord("BaseProducts","UPDATE TableProducts SET ITEM_NAME='BALL NAME UPDATED' WHERE ID=3")
+    print("\nThe record with ID=3 has been updated successfully!\n")
+
+
+    #Remove record
+    RemoveRecord("BaseProducts","DELETE FROM TableProducts WHERE ID=3")
+    print("The record with ID=4 has been removed successfully!\n")
